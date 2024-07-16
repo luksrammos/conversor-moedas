7a7d3f1a540e18c1a43a4f0fdecb01dde04dd54a
Lucas Ramos <lucas.rsilva32@gmail.com>
2024-07-15 22:09:41 -0300

    projeto final

---
+++ src/com/br/projetoconversor/model/Conversor.java
@@ -0,0 +1,8 @@
+package com.br.projetoconversor.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public record Conversor(@SerializedName("base_code") String moedaEscolhida,
+                        @SerializedName("target_code") String moedaAConverter,
+                        @SerializedName("conversion_result") double valorCotacao) {
+}
---
+++ src/com/br/projetoconversor/principal/Main.java
@@ -0,0 +1,12 @@
+package com.br.projetoconversor.principal;
+
+import com.br.projetoconversor.model.InterfaceUsuario;
+
+    public class Main {
+        public static void main(String[] args) {
+
+            InterfaceUsuario interfaceUsuario = new InterfaceUsuario();
+            interfaceUsuario.menu();
+
+        }
+    }
